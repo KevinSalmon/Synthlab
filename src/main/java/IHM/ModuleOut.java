506573bad88b2ea055610819166eccba127b6b08
mcs <marie.coisnard@gmail.com>
2018-01-31 17:49:54 +0100

    Ajout fxml et du controller associé du module Out

---
+++ src/main/java/IHM/ModuleOut.java
@@ -0,0 +1,54 @@
+package IHM;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.SpinnerValueFactory;
+
+public class ModuleOut implements Initializable {
+
+	@FXML
+	Spinner<Integer> btn_attenuateur;
+	
+	@FXML
+	CheckBox checkbox_mute;
+
+	final int initialValue = 0;
+	int minValue = Integer.MIN_VALUE;
+	int maxValue = 12;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// Value factory.
+		SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(minValue, maxValue,
+				initialValue);
+
+		btn_attenuateur.setValueFactory(valueFactory);
+		btn_attenuateur.setEditable(true);
+	}
+
+	public int getMinValue() {
+		return minValue;
+	}
+
+	public void setMinValue(int minValue) {
+		this.minValue = minValue;
+	}
+
+	public int getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(int maxValue) {
+		this.maxValue = maxValue;
+	}
+
+	public int getInitialValue() {
+		return initialValue;
+	}
+	
+}
