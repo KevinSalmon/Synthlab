bb8d39583cfa4003203dc255774efac048cc0943
Julien Laurent <digyanoen@orange.fr>
2018-02-06 15:34:50 +0100

    [US 13] Add EG

---
+++ src/main/java/module/EG.java
@@ -0,0 +1,102 @@
+package module;
+
+import com.jsyn.ports.UnitInputPort;
+import com.jsyn.ports.UnitOutputPort;
+import com.jsyn.ports.UnitPort;
+import com.jsyn.unitgen.EnvelopeDAHDSR;
+import com.jsyn.unitgen.UnitSource;
+import utils.Tuple;
+
+public class EG extends Module implements UnitSource{
+
+    private final EnvelopeDAHDSR envelope;
+
+    public UnitOutputPort out;
+
+    public UnitInputPort gate;
+
+    private double attack;
+    private double decay;
+    private double sustain;
+    private double release;
+
+    public EG() {
+        envelope = new EnvelopeDAHDSR();
+
+        add(envelope);
+
+        attack = 0.1;
+        decay = 0.2;
+        sustain = 0.5;
+        release = 0.3;
+
+        addPort(out = envelope.output, PortType.OUTPUT.getType());
+        addPort(gate = envelope.input, PortType.INPUT.getType());
+    }
+
+    public double getAttack() {
+        return attack;
+    }
+
+    public void setAttack(double attack) {
+        this.attack = attack;
+
+        if(this.attack < 0.01) this.attack = 0.01;
+        else if(this.attack > 8) this.attack = 8;
+
+        envelope.attack.set(this.attack);
+    }
+
+    public double getDecay() {
+        return decay;
+    }
+
+    public void setDecay(double decay) {
+        this.decay = decay;
+
+        if(this.decay < 0.0) this.decay = 0.0;
+        else if(this.decay > 2) this.decay = 2;
+
+        envelope.decay.set(this.decay);
+    }
+
+    public double getSustain() {
+        return sustain;
+    }
+
+    public void setSustain(double sustain) {
+        this.sustain = sustain;
+
+        if(this.sustain < 0.0) this.sustain = 0.0;
+        else if(this.sustain > 1) this.sustain = 1;
+
+        envelope.sustain.set(this.sustain);
+    }
+
+    public double getRelease() {
+        return release;
+    }
+
+    public void setRelease(double release) {
+        this.release = release;
+
+        if(this.release < 0.01) this.release = 0.01;
+        else if(this.release > 8) this.release = 8;
+
+        envelope.release.set(this.release);
+    }
+
+    @Override
+    Tuple<UnitPort, PortType> getPort(String name) {
+        return null;
+    }
+
+    @Override
+    public UnitOutputPort getOutput() {
+        return out;
+    }
+
+    public UnitInputPort getInput() {
+        return gate;
+    }
+}
