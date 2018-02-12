4ec79bed40e9a38ca0b27a64d21dd85bace38140
Unknown <nicolas.fortun@etudiant.univ-rennes1.fr>
2018-02-12 16:42:40 +0100

    Encore plus de tests

--- src/test/java/module/SequenceurTest.java
+++ src/test/java/module/SequenceurTest.java
@@ -1,6 +1,7 @@
 package module;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import utils.PortType;
 
@@ -8,10 +9,19 @@ public class SequenceurTest {
     private Sequenceur sequenceur;
 
 
+    @Before
+    public void init(){
+        sequenceur = new Sequenceur();
+    }
     @Test
     public void getPortTest(){
-        sequenceur = new Sequenceur();
         Assert.assertEquals("Ports must be equals", sequenceur.getInputPort(), sequenceur.getPort(PortType.INPUT.getType()).getLeft());
         Assert.assertEquals("Ports must be equals", sequenceur.getOutputPort(), sequenceur.getPort(PortType.OUTPUT.getType()).getLeft());
+        Assert.assertNull("Does not exist", sequenceur.getPort("fake"));
+    }
+
+    @Test
+    public void getReference(){
+        Assert.assertEquals("Must be equals", sequenceur, sequenceur.getReference());
     }
 }
