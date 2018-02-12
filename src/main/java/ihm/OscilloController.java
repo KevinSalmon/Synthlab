0cde173d083865b7fdae88c0ed0f6d6bc974a277
mcs <marie.coisnard@gmail.com>
2018-02-12 17:10:28 +0100

    Correction de l'oscillo - Possibilité de régler le refresh - Possibilité d'ajuster l'échelle

--- src/main/java/ihm/OscilloController.java
+++ src/main/java/ihm/OscilloController.java
@@ -12,16 +12,17 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.util.Duration;
 import module.Oscilloscope;
 import utils.CableManager;
 import utils.PortType;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import javafx.animation.Timeline;
 
@@ -46,10 +47,23 @@ public class OscilloController implements Initializable, SubjectOscillo{
     @FXML
     private NumberAxis yAxis;
 
-    private Line line;
-
     @FXML
     private Button delete;
+
+    @FXML
+    private Spinner<Integer> refresh;
+
+    @FXML
+    private Spinner<Integer> spinnerAxisX;
+
+    @FXML
+    private Spinner<Integer> spinnerAxisY;
+
+    private int defaultRefreshTime = 500;
+
+    private int minRefreshTime = 50;
+
+    private int maxRefreshTime = 5000;
 
     /**
      * module observateur
@@ -63,12 +77,34 @@ public class OscilloController implements Initializable, SubjectOscillo{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactoryX =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(100, Oscilloscope.TMAX, Oscilloscope.TMAX);
+
+        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactoryY =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20, 14);
+
+
+        spinnerAxisX.setValueFactory(valueFactoryX);
+        spinnerAxisY.setValueFactory(valueFactoryY);
+
+        valueFactoryX.amountToStepByProperty().setValue(50);
+
+        spinnerAxisY.setOnInputMethodTextChanged(event -> updateAxisY());
+        spinnerAxisY.setOnKeyReleased(e ->updateAxisY());
+        spinnerAxisY.setOnMouseClicked(e -> updateAxisY());
+        spinnerAxisY.setEditable(true);
+
+        spinnerAxisX.setOnInputMethodTextChanged(event -> updateAxisX());
+        spinnerAxisX.setOnKeyReleased(e ->updateAxisX());
+        spinnerAxisX.setOnMouseClicked(e -> updateAxisX());
+        spinnerAxisX.setEditable(true);
+
         xAxis.setAutoRanging(false);
         xAxis.setLowerBound(0);
         xAxis.setUpperBound(Oscilloscope.TMAX);
-        xAxis.setTickUnit(200);
-        xAxis.setSide(Side.BOTTOM);
+        xAxis.setTickUnit(50);
 
+        xAxis.setSide(Side.BOTTOM);
         yAxis.setAutoRanging(false);
         yAxis.setLowerBound(-14);
         yAxis.setUpperBound(14);
@@ -80,7 +116,37 @@ public class OscilloController implements Initializable, SubjectOscillo{
 
         lineChart.setAnimated(false);
 
+        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactoryRefresh =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(minRefreshTime, maxRefreshTime, defaultRefreshTime);
+        valueFactoryRefresh.amountToStepByProperty().setValue(20);
+
+        timeline = new Timeline();
+
+        refresh.setValueFactory(valueFactoryRefresh);
+        refresh.setEditable(true);
+        refresh.setOnInputMethodTextChanged(event -> updateRefreshTime());
+        refresh.setOnKeyReleased(e ->updateRefreshTime());
+        refresh.setOnMouseClicked(e -> updateRefreshTime());
+
         delete.setOnMouseClicked(eh -> Controller.getInstance().removeWithConfirmPopup(oscilloObseurveur, border));
+    }
+
+    private void updateAxisY() {
+        yAxis.setLowerBound(-spinnerAxisY.getValue());
+        yAxis.setUpperBound(spinnerAxisY.getValue());
+    }
+
+    private void updateAxisX(){
+        xAxis.setUpperBound(spinnerAxisX.getValue());
+    }
+
+    private void updateRefreshTime(){
+        timeline.stop();
+        timeline = new Timeline(new KeyFrame(
+                Duration.millis(refresh.getValue()),
+                ae -> oscilloObseurveur.update(this)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     @Override
@@ -89,13 +155,7 @@ public class OscilloController implements Initializable, SubjectOscillo{
             oscilloObseurveur = o;
             CableManager.getInstance().addListener(in, oscilloObseurveur.getReference(), PortType.INPUT, border);
             CableManager.getInstance().addListener(out, oscilloObseurveur.getReference(), PortType.OUTPUT, border);
-            SubjectOscillo me = this;
-
-            timeline = new Timeline(new KeyFrame(
-                    Duration.millis(2500),
-                    ae -> oscilloObseurveur.update(this)));
-            timeline.setCycleCount(Animation.INDEFINITE);
-            timeline.play();
+            updateRefreshTime();
         }
     }
 
@@ -113,13 +173,12 @@ public class OscilloController implements Initializable, SubjectOscillo{
 
     @Override
     public void receiveSeries(double[] series) {
-        XYChart.Series<Integer, Double> serie = new XYChart.Series<>();
+        lineChart.getData().clear();
+                XYChart.Series<Integer, Double> serie = new XYChart.Series<>();
         serie.setName("Values");
         for(int i = 0 ; i < series.length ; i++){
             serie.getData().add(new XYChart.Data(i, series[i]));
         }
-
-        lineChart.getData().clear();
         lineChart.getData().add(serie);
     }
 
